39f881887dc45f3aa4b08e9d385720b7c8a8ecfd
Nehatkr <nehathakur2308@gmail.com>
2025-06-19 21:51:11 +0530

    day13

---
+++ Number/sum_of_ap.cpp
@@ -0,0 +1,21 @@
+// Given an A.P. Series, we need to find the sum of the Series.
+
+#include <bits/stdc++.h>
+using namespace std;
+float sumofAp(float a, float d, int n)
+{
+    float sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += a;
+        a += d;
+    }
+    return sum;
+}
+int main()
+{
+    float a = 1.5, d = 3;
+    int n = 5;
+    cout << "Sum of Given Ap Series: " << sumofAp(a, d, n) << endl;
+    return 0;
+}
